5661e1ee82b54e3cef664af7109772fbca489223
Lara Corsini <laracorsinidemiranda@gmail.com>
2025-09-27 20:38:32 -0300

    init: setting up dev environment

---
+++ cozinha-aberta/src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-5xl font-bold text-blue-600">
+          Tailwind funcionando! 🎉
+        </h1>
+        <button className="mt-6 px-4 py-2 rounded-lg bg-purple-500 text-white hover:bg-purple-600">
+          Teste botão
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
